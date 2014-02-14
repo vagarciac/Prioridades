7dc44ed755c9bf0f3ce3476b2652f1425bacb5a3
Victor Garcia <vagarciac@gmail.com>
2014-02-13 21:11:28 -0600

    Configuración del connectionString
    Configuración del connectionString para accesar la BD localmente.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,11 +14,11 @@ public partial class _Default : System.Web.UI.Page
 
     protected void gvMaestro_PreRender(object sender, EventArgs e)
     {
-        SqlDataSource2.ConnectionString = "Data Source=" + ddlIP.SelectedItem.Text + "\\SQLEXPRESS;Initial Catalog=Turnos;User ID=writer;Password=Wr1ter";
+        //SqlDataSource2.ConnectionString = "Data Source=" + ddlIP.SelectedItem.Text + "\\SQLEXPRESS;Initial Catalog=Turnos;User ID=writer;Password=Wr1ter";
     }
 
     protected void dvDetalle_ItemCreated(object sender, EventArgs e)
     {
-        SqlDataSource3.ConnectionString = "Data Source=" + ddlIP.SelectedItem.Text + "\\SQLEXPRESS;Initial Catalog=Turnos;User ID=writer;Password=Wr1ter";
+        //SqlDataSource3.ConnectionString = "Data Source=" + ddlIP.SelectedItem.Text + "\\SQLEXPRESS;Initial Catalog=Turnos;User ID=writer;Password=Wr1ter";
     }
 }
